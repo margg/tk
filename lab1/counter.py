f2ed2f059230630f231b83287013489407255d93
marg <mksalawa@gmail.com>
2015-10-17 00:48:03 +0200

    find author (2x regex)

--- lab1/counter.py
+++ lab1/counter.py
@@ -7,12 +7,22 @@ def processFile(filepath):
     fp = codecs.open(filepath, 'rU', 'iso-8859-2')
     content = fp.read()
 
-#
-#  INSERT YOUR CODE HERE
-#
+    meta = re.findall(r"(<META [^>]*>)", content, re.DOTALL | re.IGNORECASE)
+    meta.append("<META CONTENT=\"Jacek Czarnecki\" NAME=\"AUTOR\">")
+
+    nameAttr = r"(?P<name>NAME(?:\s*)=(?:\s*)\"AUTOR\")"
+    contentAttr = r"(?P<content>CONTENT(?:\s*)=(?:\s*)\"(?P<autor>[^\"]*)\")"
+    authorPattern1 = re.compile(nameAttr + r"(?P<other>.*)" + contentAttr, re.DOTALL | re.IGNORECASE)
+    authorPattern2 = re.compile(contentAttr + r"(?P<other>.*)" + nameAttr, re.DOTALL | re.IGNORECASE)
+
+    for m in meta:
+        mat = authorPattern1.search(m) or authorPattern2.search(m)
+        if mat:
+            author = mat.group('autor')
+
 
     fp.close()
-    print("nazwa pliku:", filepath)
+    print("nazwa pliku: {0}".format(filepath))
     print("autor:")
     print("dzial:")
     print("slowa kluczowe:")
@@ -25,17 +35,19 @@ def processFile(filepath):
     print("\n")
 
 
-try:
-    path = sys.argv[1]
-except IndexError:
-    print("Brak podanej nazwy katalogu")
-    sys.exit(0)
+# try:
+#     path = sys.argv[1]
+# except IndexError:
+#     print("Brak podanej nazwy katalogu")
+#     sys.exit(0)
+#
+#
+# tree = os.walk(path)
+#
+# for root, dirs, files in tree:
+#     for f in files:
+#         if f.endswith(".html"):
+#             filePath = os.path.join(root, f)
+#             processFile(filePath)
 
-
-tree = os.walk(path)
-
-for root, dirs, files in tree:
-    for f in files:
-        if f.endswith(".html"):
-            filePath = os.path.join(root, f)
-            processFile(filePath)
+processFile("1993-01/199301030001.html")
