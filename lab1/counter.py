36a6b85e2cd9e7c08a1c409ae2c2ec4aad3c4226
marg <mksalawa@gmail.com>
2015-10-24 20:16:21 +0200

    floats

--- lab1/counter.py
+++ lab1/counter.py
@@ -18,12 +18,17 @@ def process_file(filepath):
     keywords = find_meta(content, "kluczowe_\d+")
 
     article = re.search(r"<p>(.*?)<meta\s", content, re.IGNORECASE | re.DOTALL)
-    article = article.group(1) if article else ""
+    article = article.group(1) if article else content
     sentences = []
     abbreviations = set(re.findall(r"(?:^|\s|\b)+([a-z]{1,3}\.)(?:$|\s|\b)+", article, re.IGNORECASE))
     # -32768 - 32767, przed: bialy znak, po: bialy znak lub kropka i bialy znak
-    integers = re.findall(
-        r"(\s|^)((-?)(\d{1,4}|[1-2]\d{4}|31\d{3}|32[1-6]\d{2}|327[1-5]\d|3276[1-7])|-32768)((\s)|\.\s|$)", article)
+    integers = map(lambda x: x[1], re.findall(
+        r"(\s|^)((-?)(\d{1,4}|[1-2]\d{4}|31\d{3}|32[1-6]\d{2}|327[1-5]\d|3276[1-7])|-32768)((\s)|\.(\s|$)|$)", article))
+
+    floats = map(lambda x: x[2], re.findall(r"((\s|^)([1-9]\d*\.\d+(e(\+|-)\d+)?))((\s)|\.(\s|$)|$)",
+                                            article, re.IGNORECASE | re.MULTILINE))
+
+
 
     fp.close()
     print("nazwa pliku: {0}".format(filepath))
@@ -60,10 +65,14 @@ def process_file(filepath):
 
     print("liczba liczb calkowitych z zakresu int: {0}\n\t".format(len(integers))),
     for i in integers:
-        print("{0},".format(i[1])),
+        print("{0},".format(i)),
     print
 
-    print("liczba liczb zmiennoprzecinkowych:")
+    print("liczba liczb zmiennoprzecinkowych: {0}\n\t".format(len(floats))),
+    for f in floats:
+        print("{0},".format(f)),
+    print
+
     print("liczba dat:")
     print("liczba adresow email:")
     print("\n")
