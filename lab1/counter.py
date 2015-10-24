bdd5c192554c0491cd8b159ecf86cf288ca04542
marg <mksalawa@gmail.com>
2015-10-24 14:55:36 +0200

    skroty

--- lab1/counter.py
+++ lab1/counter.py
@@ -16,39 +16,65 @@ def process_file(filepath):
     dzialy = find_meta(content, "dzial")
     kluczowe = find_meta(content, "kluczowe_.")
 
-    fp.close()
+    zdania = []
 
+    skroty = re.findall(r"[ ^]+([a-z]{1,3}\.)[ \n$]+", content, re.IGNORECASE | re.MULTILINE)
+
+
+
+    fp.close()
     print("nazwa pliku: {0}".format(filepath))
-    for autor in autorzy:
-        print("autor: {0}".format(autor))
-    for dzial in dzialy:
-        print("dzial: {0}".format(dzial))
+    try:
+        for autor in autorzy:
+            print("autor: {0}".format(autor))
+    except UnicodeEncodeError:      # problem przy wypisywaniu polskich znakow
+        print "Unicode Encode Error\n"
+        pass
+
+    try:
+        for dzial in dzialy:
+            print("dzial: {0}".format(dzial))
+    except UnicodeEncodeError:
+        print "Unicode Encode Error\n"
+        pass
+
     print("slowa kluczowe: "),
-    for klucz in kluczowe:
-        print("{0}, ".format(klucz)),
-    print
+    try:
+        for klucz in kluczowe:
+            if klucz:   # pomin puste slowa kluczowe
+                print("{0},".format(klucz)),
+        print
+    except UnicodeEncodeError:
+        print "Unicode Encode Error\n"
+        pass
+
     print("liczba zdan:")
-    print("liczba skrotow:")
+
+    skroty = set(skroty)
+    print("liczba skrotow: {0}\n\t".format(len(skroty))),
+    for s in skroty:
+        print("{0},".format(s)),
+    print
     print("liczba liczb calkowitych z zakresu int:")
     print("liczba liczb zmiennoprzecinkowych:")
     print("liczba dat:")
     print("liczba adresow email:")
     print("\n")
+    print("======================================================\n")
 
 
-# try:
-#     path = sys.argv[1]
-# except IndexError:
-#     print("Brak podanej nazwy katalogu")
-#     sys.exit(0)
-#
-#
-# tree = os.walk(path)
-#
-# for root, dirs, files in tree:
-#     for f in files:
-#         if f.endswith(".html"):
-#             filePath = os.path.join(root, f)
-#             processFile(filePath)
+try:
+    path = sys.argv[1]
+except IndexError:
+    print("Brak podanej nazwy katalogu")
+    sys.exit(0)
 
-process_file("1993-01/199301040001.html")
+
+tree = os.walk(path)
+
+for root, dirs, files in tree:
+    for f in files:
+        if f.endswith(".html"):
+            filePath = os.path.join(root, f)
+            process_file(filePath)
+
