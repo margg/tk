0cfa51fbd1579b3075dbbf428731bc35e23a89b7
Piotr Roksela <piotr@roksela.pl>
2015-11-28 17:33:43 +0100

    added ttypes

--- lab3/TypeChecker.py
+++ lab3/TypeChecker.py
@@ -7,22 +7,22 @@ class NodeVisitor(object):
 
     def visit(self, node):
         method = 'visit_' + node.__class__.__name__
+        print(method)
         visitor = getattr(self, method, self.generic_visit)
-        print("visit of {0}", node.__class__.__name__)
         return visitor(node)
 
     def generic_visit(self, node):        # Called if no explicit visitor function exists for a node.
-        # if isinstance(node, list):
-        #     for elem in node:
-        #         self.visit(elem)
-        # else:
-        #     for child in node.children:
-        #         if isinstance(child, list):
-        #             for item in child:
-        #                 if isinstance(item, AST.Node):
-        #                     self.visit(item)
-        #         elif isinstance(child, AST.Node):
-        #             self.visit(child)
+        if isinstance(node, list):
+            for elem in node:
+                self.visit(elem)
+        else:
+            for child in node.children:
+                if isinstance(child, list):
+                    for item in child:
+                        if isinstance(item, AST.Node):
+                            self.visit(item)
+                elif isinstance(child, AST.Node):
+                    self.visit(child)
         print("generic visit of {0}", node.__class__.__name__)
 
     # simpler version of generic_visit, not so general
@@ -32,6 +32,80 @@ class NodeVisitor(object):
 
 
 class TypeChecker(NodeVisitor):
+
+    def __init__(self):
+        self.correct_ttypes = {}
+        self.fill_ttypes()
+
+    def fill_ttypes(self):
+        # arithmetic int operations
+        self.add_ttype('+','int','int','int')
+        self.add_ttype('-','int','int','int')
+        self.add_ttype('*','int','int','int')
+        self.add_ttype('/','int','int','int')
+        self.add_ttype('%','int','int','int')
+        # binary int operations
+        self.add_ttype('&','int','int','int')
+        self.add_ttype('|','int','int','int')
+        self.add_ttype('^','int','int','int')
+        self.add_ttype('<<','int','int','int')
+        self.add_ttype('>>','int','int','int')
+        # arithmetic float operations
+        self.add_ttype('+','float','float','float')
+        self.add_ttype('-','float','float','float')
+        self.add_ttype('*','float','float','float')
+        self.add_ttype('/','float','float','float')
+        self.add_ttype('%','float','float','float')
+        self.add_ttype('+','int','float','float')
+        self.add_ttype('-','int','float','float')
+        self.add_ttype('*','int','float','float')
+        self.add_ttype('/','int','float','float')
+        self.add_ttype('%','int','float','float')
+        self.add_ttype('+','float','int','float')
+        self.add_ttype('-','float','int','float')
+        self.add_ttype('*','float','int','float')
+        self.add_ttype('/','float','int','float')
+        self.add_ttype('%','float','int','float')
+        # relational int operations
+        self.add_ttype('==','int','int','int')
+        self.add_ttype('!=','int','int','int')
+        self.add_ttype('<','int','int','int')
+        self.add_ttype('>','int','int','int')
+        self.add_ttype('<=','int','int','int')
+        self.add_ttype('>=','int','int','int')
+        # relational float operations
+        self.add_ttype('==','float','float','float')
+        self.add_ttype('!=','float','float','float')
+        self.add_ttype('<','float','float','float')
+        self.add_ttype('>','float','float','float')
+        self.add_ttype('<=','float','float','float')
+        self.add_ttype('>=','float','float','float')
+        self.add_ttype('==','int','float','float')
+        self.add_ttype('!=','int','float','float')
+        self.add_ttype('<','int','float','float')
+        self.add_ttype('>','int','float','float')
+        self.add_ttype('<=','int','float','float')
+        self.add_ttype('>=','int','float','float')
+        self.add_ttype('==','float','int','float')
+        self.add_ttype('!=','float','int','float')
+        self.add_ttype('<','float','int','float')
+        self.add_ttype('>','float','int','float')
+        self.add_ttype('<=','float','int','float')
+        self.add_ttype('>=','float','int','float')
+        # string operations
+        self.add_ttype('+','string','string','string')
+        self.add_ttype('+','string','int','string')
+        self.add_ttype('==','string','string','string')
+        self.add_ttype('!=','string','string','string')
+        self.add_ttype('<','string','string','string')
+        self.add_ttype('>','string','string','string')
+        self.add_ttype('<=','string','string','string')
+        self.add_ttype('>=','string','string','string')
+
+
+
+    def add_ttype(self, operation, operand1, operand2, returned):
+        self.correct_ttypes[operation][operand1][operand2] = returned
 
     def visit_Program(self, node):
         pass
@@ -51,6 +125,14 @@ class TypeChecker(NodeVisitor):
     #     # ...
     #     #
     #
+
+    def visit_Const(self, node):
+        # if type(node.value) == str:
+        #     self.visit(node.value)
+        # elif type(node.value) == int:
+        #     self.visit(node.value)
+        # elif type(node.value) == float:
+        #     self.visit(node.value)
 
     def visit_Integer(self, node):
         return 'int'
