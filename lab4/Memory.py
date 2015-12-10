943129585603112a2b181a0127a0cd52d6a6ca58
marg <mksalawa@gmail.com>
2015-12-10 18:41:32 +0100

    reformat

--- lab4/Memory.py
+++ lab4/Memory.py
@@ -1,14 +1,11 @@
-
-
 class Memory:
-
-    def __init__(self, name): # memory name
+    def __init__(self, name):  # memory name
         pass
 
     def has_key(self, name):  # variable name
         pass
 
-    def get(self, name):         # gets from memory current value of variable <name>
+    def get(self, name):  # gets from memory current value of variable <name>
         pass
 
     def put(self, name, value):  # puts into memory current value of variable <name>
@@ -16,21 +13,20 @@ class Memory:
 
 
 class MemoryStack:
-
-    def __init__(self, memory=None): # initialize memory stack with memory <memory>
+    def __init__(self, memory=None):  # initialize memory stack with memory <memory>
         pass
 
-    def get(self, name):             # gets from memory stack current value of variable <name>
+    def get(self, name):  # gets from memory stack current value of variable <name>
         pass
 
-    def insert(self, name, value): # inserts into memory stack variable <name> with value <value>
+    def insert(self, name, value):  # inserts into memory stack variable <name> with value <value>
         pass
 
-    def set(self, name, value): # sets variable <name> to value <value>
+    def set(self, name, value):  # sets variable <name> to value <value>
         pass
 
-    def push(self, memory): # pushes memory <memory> onto the stack
+    def push(self, memory):  # pushes memory <memory> onto the stack
         pass
 
-    def pop(self):          # pops the top memory from the stack
+    def pop(self):  # pops the top memory from the stack
         pass
