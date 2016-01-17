55ef8085167e13db1a5a0b2f5707221c9d366f2b
roxel <piotr@roksela.pl>
2016-01-17 10:03:30 +0100

    simplify: NodeList

--- lab5/src/main/scala/simplifier/Simplifier.scala
+++ lab5/src/main/scala/simplifier/Simplifier.scala
@@ -10,8 +10,32 @@ object Simplifier {
   def simplify(node: Node): Node = node match {
 
     // recognize tuples: (x,y)+(u,v) => (x, y, u, v)
-    case BinExpr("+", Tuple(a), Tuple(b)) => Tuple(a ++ b)
+    case BinExpr("+", Tuple(a), Tuple(b)) => Tuple((a ++ b) map simplify)
 
+    // simplify if-else instruction with known condition
+    case IfElseInstr(cond, left, right) =>
+      val cond_simplified = simplify(cond)
+      cond_simplified match {
+        case TrueConst() => simplify(left)
+        case FalseConst() => simplify(right)
+        case _ => IfElseInstr(cond_simplified, simplify(left), simplify(right))
+      }
+
+    // simplify if-else expression with known condition
+    case IfElseExpr(cond, left, right) =>
+      val cond_simplified = simplify(cond)
+      cond_simplified match {
+        case TrueConst() => simplify(left)
+        case FalseConst() => simplify(right)
+        case _ => IfElseExpr(cond_simplified, simplify(left), simplify(right))
+      }
+
+    case NodeList(list) =>
+      val list_simplified = list map simplify
+      if (list_simplified.size == 1) list_simplified.head else NodeList(list_simplified)
+
+    // other cases that do not change nodes
+    case x => x
   }
 
 }
