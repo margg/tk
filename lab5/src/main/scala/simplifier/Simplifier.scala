476e65a88dec9b76da5efe4bab4a7e7974083af2
roxel <piotr@roksela.pl>
2016-01-17 14:17:38 +0100

    simplify: WhileInstr, Unary and some expressions

--- lab5/src/main/scala/simplifier/Simplifier.scala
+++ lab5/src/main/scala/simplifier/Simplifier.scala
@@ -30,6 +30,51 @@ object Simplifier {
         case _ => IfElseExpr(cond_simplified, simplify(left), simplify(right))
       }
 
+    // remove while loop with False condition
+    case WhileInstr(cond, body) =>
+      val cond_simplified = simplify(cond)
+      cond_simplified match {
+        case FalseConst() => NodeList(List())
+        case _ => WhileInstr(cond_simplified, simplify(body))
+      }
+
+    // simplify expressions
+    case BinExpr("+", left, right) => (simplify(left), simplify(right)) match {
+      case (a, IntNum(b)) if b == 0 => a
+      case (IntNum(a), b) if a == 0 => b
+      case (Unary("-", Variable(a)), Variable(b)) =>
+        simplify(BinExpr("-", Variable(b), Variable(a)))
+      case (a, b) => BinExpr("+", simplify(a), simplify(b))
+    }
+
+    case BinExpr("-", left, right) => (simplify(left), simplify(right)) match {
+      case (IntNum(a), IntNum(b)) if a == b => IntNum(0)
+      case (a, b) => BinExpr("+", simplify(a), simplify(b))
+    }
+
+    case BinExpr(op, left, right) => (op, simplify(left), simplify(right)) match {
+      case (_, a, b) => BinExpr(op, a, b)
+    }
+
+    // cancel double unary ops & get rid of not before comparisons
+    case Unary(op, expr) => (op, simplify(expr)) match {
+      case ("not", Unary("not", expr1)) => simplify(expr1)
+      case ("not", BinExpr("==", left, right)) =>
+        simplify(BinExpr("!=", left, right))
+      case ("not", BinExpr("!=", left, right)) =>
+        simplify(BinExpr("==", left, right))
+      case ("not", BinExpr(">", left, right)) =>
+        simplify(BinExpr("<=", left, right))
+      case ("not", BinExpr("<", left, right)) =>
+        simplify(BinExpr(">=", left, right))
+      case ("not", BinExpr(">=", left, right)) =>
+        simplify(BinExpr("<", left, right))
+      case ("not", BinExpr("<=", left, right)) =>
+        simplify(BinExpr(">", left, right))
+      case ("-", Unary("-", expr1)) => simplify(expr1)
+      case (_, expr1) => Unary(op, simplify(expr1))
+    }
+
     case NodeList(list) =>
       val list_simplified = list map simplify
       if (list_simplified.size == 1) list_simplified.head else NodeList(list_simplified)
